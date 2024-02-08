66490c7c3dff932c6264e65c35294b8a532bfa43
igor-devloper <wagnerigor9@gmail.com>
2024-02-07 21:45:26 -0300

    release

--- next.config.js
+++ next.config.js
@@ -2,14 +2,25 @@
 const nextConfig = {}
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
 
-    return config
-  }
-}
+  // Can be safely removed in newer versions of Next.js
+  future: {
+
+    // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+    webpack5: true,   
+  },
+
+  webpack(config) {
+    config.resolve.fallback = {
+
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,  
+
+      fs: false, // the solution
+    };
+    
+    return config;
+  },
+};
