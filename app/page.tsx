166171f45d503eb771366a8e0d5e006e9a0ebbdd
igor-devloper <wagnerigor9@gmail.com>
2024-02-07 21:15:22 -0300

    release

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import Head from "@/components/main/head";
-import Projects from "@/components/main/projects";
-import Image from "next/image";
+import Hero from "@/components/main/hero";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
-        <Head />
-        {/* <Skills /> */}
-        {/* <Encryption /> */}
-        {/* <Projects /> */}
+        <Hero />
       </div>
     </main>
   );
