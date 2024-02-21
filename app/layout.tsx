6ff4dbc9b0b5befc9d4012ac511610702d2c5935
igor-devloper <wagnerigor9@gmail.com>
2024-02-21 00:00:08 -0300

    release

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,15 @@ import { Toaster } from "@/components/ui/toaster"
 import "./globals.css";
 import StarsCanvas from "@/components/main/star-background";
 import Footer from "@/components/main/Footer";
-import { useEffect } from "react";
-// import { FacebookPixel } from "../components/facebook-pixel";
-import Script from "next/script";
-import { Head } from "next/document";
 import { AppContext } from "@/context";
-// import { FB_PIXEL_ID } from "../lib/fbpixel";
 
-const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-})
+// const inter = Inter({ subsets: ["latin"] });
+// const poppins = Poppins({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-poppins',
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+// })
 
 export const metadata: Metadata = {
   title: "iDo - Markenting Digital",
@@ -33,9 +28,8 @@ export default function RootLayout({
 }) {
   return (
     <AppContext>
-      <html lang="en">
-        <Script id='fb-pixel' strategy='afterInteractive'>
-          {`
+      {/* <Script id='fb-pixel' strategy='afterInteractive'>
+        {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -48,24 +42,18 @@ export default function RootLayout({
             fbq('track', 'PageView');
             
           `}
-        </Script>
-        <body className={`${poppins.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-          <noscript>
-            <img
-              height='1'
-              width='1'
-              style={{display: 'none'}}
-              alt={'facebook pixel no script image'}
-              src='https://www.facebook.com/tr?id=1057158705375107&ev=PageView&noscript=1'
-            />
-          </noscript>
-
-          <main className="relative flex min-h-screen flex-col space-y-20">
+      </Script> */}
+      <html lang="en">
+        <body
+          className={`bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        >
+        <main className="relative flex min-h-screen flex-col space-y-20">
             <StarsCanvas />
             {children}
             <Footer />
           </main>
           <Toaster />
+          {/* <FacebookPixel/> */}
         </body>
       </html>
     </AppContext>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Hero from '../components/main/hero';
 import { Head } from 'next/document';
-import { FacebookPixel } from '@/components/faceboo-pixel';
+// import { FacebookPixel } from '@/lib/faceboo-pixel';
 
 
 
@@ -12,14 +12,6 @@ import { FacebookPixel } from '@/components/faceboo-pixel';
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover" />
-      </Head>
-      <FacebookPixel />
-
       <main className="h-full w-full scroll-mx-0">
         <div className="flex flex-col gap-20">
           <Hero />
